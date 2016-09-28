3c53e9a1408656dc39f453e9770dbf1d43af26a3
Mike Evans <mike.evans@numical.com>
2016-09-28 13:48:47 +0100

    functionality plus test

---
+++ spec/test.js
@@ -0,0 +1,10 @@
+/* eslint-env jasmine */
+'use strict';
+
+const version = require('../index.js');
+
+describe('Tests', () => {
+  it('returns correct version', () => {
+    expect(version).toEqual('0.0.1');
+  });
+});
